387007fe392ed3a0315a45e4c924d32a3f9ab679
Anoop Felix <anooptiger@hotmail.com>
2021-06-01 11:28:39 +0800

    testing

--- src/listeners/client/MessageListener.ts
+++ src/listeners/client/MessageListener.ts
@@ -1,4 +1,4 @@
-import { Message } from "discord.js";
+import { Message, TextChannel } from "discord.js";
 import { Listener } from "discord-akairo";
 import boostResponses from "../../assets/json/boostresponses.json";
 import { sprintf } from "../../";
@@ -19,6 +19,8 @@ export default class MessageListener extends Listener {
             const s = boostResponses[Math.floor(Math.random() * boostResponses.length)];
             sprintf(s, message.author.toString());
             console.log(s);
+            const target = message.guild.channels.cache.get("814485432959500308");
+            (target as TextChannel).send(s);
         }
     }
 }
