73840c2a21e76274a6cf572d19854146a3a8e83b
Anoop Felix <anooptiger@hotmail.com>
2021-06-06 23:22:26 +0800

    typo

--- src/listeners/commandHandler/MissingPermissionsListener.ts
+++ src/listeners/commandHandler/MissingPermissionsListener.ts
@@ -16,7 +16,7 @@ export default class MissingPermissionsListener extends Listener {
     public async exec(message: Message, command: Command, type: string, missing: any): Promise<Message> {
         this.client.log.info(`${message.author.tag} (${message.author.id}) missing permissions to call ${command.id} (${Object.values(missing).join(", ")})`);
         return message.channel.send(new ArchonEmbed()
-            .setDescription(`You are missing permission to use this command. (${Object.values(missing).join(", ")}).`)
+            .setDescription(`You are missing permissions to use this command: (${Object.values(missing).join(", ")}).`)
         );
     }
 }
